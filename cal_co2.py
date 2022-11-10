d5fa3d3cd5a391b6a9599217faa785ca7e352f80
Helena Canever <49685784+HelenaCanever@users.noreply.github.com>
2022-11-10 17:42:41 +0100

    Update cal_co2.py
    - added ml function
    - updated stockage function

--- cal_co2.py
+++ cal_co2.py
@@ -29,9 +29,23 @@ def visio(camera_on, h_visio, data, outil_visio, mois):
 		em_visio = (h_visio*4*mois*float(data.loc[data["Outil"]==outil_visio, "kgCO2eq/h Video+Audio"]))/1000*0.29
 	return em_visio
 
-def stockage(tb_year, n_backups, mois, retention_years, w, pue, f):
-	em_stockage = (tb_year*(1+n_backups) *((mois/12)*retention_years))*(w/1000)*8760*pue*(f*1000)
+def stockage(tb_year, n_backups, mois, retention_years, w, pue, f, offset, offset_ratio):
+	if offset:
+		percentage = offset_ratio/100
+		em_stockage = ((tb_year*(1+n_backups) *((mois/12)*retention_years))*(w/1000)*8760*pue*(f*1000))* (1-percentage)
+	else:
+		em_stockage = (tb_year*(1+n_backups) *((mois/12)*retention_years))*(w/1000)*8760*pue*(f*1000)
 	return em_stockage
+
+def ml(gpu_data, cloud_data, h_gpu, gpu, provider_gpu, zone_gpu, offset):
+	kWh = (h_gpu * float((gpu_data.loc[gpu_data["name"]==gpu, "tdp_watts"])))/1000
+	CO2_per_kWh = float(cloud_data.loc[(cloud_data["providerName"]==provider_gpu)&(cloud_data["region"]==zone_gpu), "impact"])
+	if offset:
+		percentage = float(cloud_data.loc[(cloud_data["providerName"]==provider_gpu)&(cloud_data["region"]==zone_gpu), "offsetRatio"])/100
+		em_ml = ((kWh * CO2_per_kWh)/1000) * (1-percentage)
+	else:
+		em_ml = (kWh * CO2_per_kWh)/1000
+	return em_ml
 
 def impressions(recto_verso, n_impression):
 	if recto_verso:
