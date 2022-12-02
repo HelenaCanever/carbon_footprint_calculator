507ad310c14c5ffade7d3db198aba7b7993eb04a
Helena Canever <49685784+HelenaCanever@users.noreply.github.com>
2022-12-02 14:35:35 +0100

    Update pdf_export.py

--- pdf_export.py
+++ pdf_export.py
@@ -16,9 +16,10 @@ def createpdf(data, co2_transport, co2_digital, co2_office):
     class PDF(FPDF):
         def __init__(self):
             super().__init__()
-            self.add_font('MontserratBlack', '', fname=os.path.abspath(r"fonts\" + "Montserrat-Black.ttf"), uni=True)
+            fpdf.set_global("SYSTEM_TTFONTS", os.path.join(os.path.dirname(''),FONTS_DIR))
+            self.add_font('MontserratBlack', '', fname=os.path.abspath("fonts/" + "Montserrat-Black.ttf"), uni=True)
             
-            self.add_font('MontserratLight', '', fname=os.path.abspath(r"fonts\" + "Montserrat-Light.ttf"), uni=True)
+            self.add_font('MontserratLight', '', fname=os.path.abspath("fonts/" + "Montserrat-Light.ttf"), uni=True)
         def header(self):
             self.set_font('MontserratBlack', '', 12)
             self.cell(0, 15, 'Bilan carbone', 1, 1, 'C')
